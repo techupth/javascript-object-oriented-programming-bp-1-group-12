802cb1cd0c0f246c15be8dc5863aee7dff226d8c
Natakorn Santhip <1st.natakorn@gmail.com>
2023-10-23 22:49:14 +0700

    finish Assignment: Object Oriented Programming

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,40 @@
-class Notification { }
-
-class EmailNotification { }
-
-class SMSNotification { }
+class Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+      this.notificationId = notificationId;
+      this.createdTime = createdTime;
+      this.content = content;
+      this.receiver = receiver;
+    }
+  
+    send() {
+      console.log(`Notification has been sent to ${this.receiver}`);
+    }
+  }
+  
+class EmailNotification extends Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+      super(notificationId, createdTime, content, receiver);
+    }
+  }
+  
+class SMSNotification extends Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+      super(notificationId, createdTime, content, receiver);
+    }
+  }
+  
+const emailNotification = new EmailNotification(
+    "001",
+    "2023-10-08 10:00",
+    "Hello, this is an email notification",
+    "example@example.com"
+  );
+  emailNotification.send();
+  
+const smsNotification = new SMSNotification(
+    "002",
+    "2023-10-08 10:15",
+    "Hello, this is an SMS notification",
+    "1234567890"
+  );
+  smsNotification.send();
