7bc7e56ea807b4745bee6c0a10a71d766c6106e6
Kawin <kawind7@gmail.com>
2023-10-23 21:40:56 +0700

    Object Oriented Programming #1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,42 @@
-class EmailNotification { }
-
-class SMSNotification { }
+class EmailNotification {
+    constructor(notificationId, createdTime, content, receiver) {
+      this.notificationId = notificationId;
+      this.createdTime = createdTime;
+      this.content = content;
+      this.receiver = receiver;
+    }
+  
+    send() {
+      console.log(`Notification has been sent to ${this.receiver}`);
+    }
+  }
+  
+class SMSNotification {
+    constructor(notificationId, createdTime, content, phoneNumber) {
+      this.notificationId = notificationId;
+      this.createdTime = createdTime;
+      this.content = content;
+      this.phoneNumber = phoneNumber;
+    }
+  
+    send() {
+      console.log(`Notification has been sent to ${this.phoneNumber}`);
+    }
+  }
+  
+  const emailNotification = new EmailNotification(
+    "001",
+    "2023-10-08 10:00",
+    "Hello, this is an email notification",
+    "example@example.com"
+  );
+  emailNotification.send();
+  
+  const smsNotification = new SMSNotification(
+    "002",
+    "2023-10-08 10:15",
+    "Hello, this is an SMS notification",
+    "1234567890"
+  );
+  smsNotification.send();
+  
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,107 @@
 //  Start coding here
+class User {
+    constructor(id, name, email) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+    }
+  }
+  
+class PostList {
+    constructor() {
+      this.posts = [];
+    }
+  
+    addPost(post) {
+      this.posts.push(post);
+    }
+  
+    sharePost(post) {
+      console.log(`You've shared post "${post.title}" to your friend.`);
+    }
+  }
+
+class Post {
+    constructor(id, title, content) {
+      this.id = id;
+      this.title = title;
+      this.content = content;
+      this.comments = [];
+  
+      this.addComment = (comment) => {
+        this.comments.push(comment);
+      };
+    }
+  }
+
+class Comment {
+    constructor(id, content, createdBy) {
+      this.id = id;
+      this.content = content;
+      this.createdBy = createdBy;
+      this.like = 0;
+    }
+  
+    addLike() {
+      this.like++;
+    }
+  }
+  
+class Facebook {
+    constructor() {
+      this.groupList = [];
+      this.pageList = [];
+    }
+  
+    addGroup(group) {
+      this.groupList.push(group);
+    }
+  
+    addPage(page) {
+      this.pageList.push(page);
+    }
+  }
+
+class FacebookPage {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+    
+class FacebookGroup {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+class Notification {
+    constructor(id) {
+      this.id = id;
+    }
+  
+    send(commentCreatedBy, postTitle) {
+      console.log(`Notification: ${commentCreatedBy} has just commented on this post—"${postTitle}"`);
+    }
+  }
+  
+  // Create instances and test the classes
+  const user = new User("user1", "John", "john@example.com");
+  const post = new Post("post1", "My First Post", "This is the content of my first post.");
+  const comment = new Comment("comment1", "Great post!", "Alice");
+  
+  post.addComment(comment);
+  
+  const postList = new PostList();
+  postList.addPost(post);
+  postList.sharePost(post);
+  
+  const group = new FacebookGroup("Web Developers");
+  const page = new FacebookPage("Tech News");
+  
+  const facebook = new Facebook();
+  facebook.addGroup(group);
+  facebook.addPage(page);
+  
+  const notification = new Notification("notification1");
+  notification.send("Alice", "My First Post");
+  
